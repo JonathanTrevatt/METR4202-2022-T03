85b1b018ef05736ba62b1e6f13b1b1700b4853f1
Jonathan Trevatt <jonathan+github@trevatt.net>
2022-09-05 14:41:29 +1000

    Implemented pose estimation, added method to convert a float lists to int tuples (required for converting 2d point lists from aruco library for use in cv2 library

--- detect_aruco_image.py
+++ detect_aruco_image.py
@@ -5,6 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+import math
 
 # construct the command line argument parser and parse the arguments
 # --image: The path to the input image containing any ArUco tags we want to detect
@@ -75,33 +76,48 @@ def extract_marker_corners(markerCorner):
     corners = [topRight, bottomRight, bottomLeft, topLeft]
     return corners
 
+def float_lists_to_int_tuples(f_lists):
+    for i in range(len(f_lists)):
+        f_lists[i] = tuple(map(int, f_lists[i]))
+    return f_lists
+
 
 def draw_quad(image, topLeft, topRight, bottomRight, bottomLeft):
+    (topLeft, topRight, bottomRight, bottomLeft) = float_lists_to_int_tuples([topLeft, topRight, bottomRight, bottomLeft])
+
     cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
     cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
     cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
     cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
     return image
 
+
 def draw_quad_centerpoint(image, topLeft, topRight, bottomRight, bottomLeft):
+    (topLeft, topRight, bottomRight, bottomLeft) = float_lists_to_int_tuples([topLeft, topRight, bottomRight, bottomLeft])
     # compute and draw the center (x, y)-coordinates of the ArUco marker
     (cX, cY) = get_quad_centerpoint(topLeft, topRight, bottomRight, bottomLeft)
     cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
     # draw the ArUco marker ID on the image
     return image
 
+
 def draw_quad_markerID(image, topLeft, topRight, bottomRight, bottomLeft, markerID):
+    (topLeft, topRight, bottomRight, bottomLeft) = float_lists_to_int_tuples([topLeft, topRight, bottomRight, bottomLeft])
     # draw the ArUco marker ID on the image
     cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     print("[INFO] ArUco marker ID: {}".format(markerID))
     return image
 
+
 def get_quad_centerpoint(topLeft, topRight, bottomRight, bottomLeft):
+    (topLeft, topRight, bottomRight, bottomLeft) = float_lists_to_int_tuples([topLeft, topRight, bottomRight, bottomLeft])
     cX = int((topLeft[0] + bottomRight[0]) / 2.0)
     cY = int((topLeft[1] + bottomRight[1]) / 2.0)
     return cX, cY
 
+
 def warp_quad_to_square(image, topLeft, topRight, bottomRight, bottomLeft):
+    (topLeft, topRight, bottomRight, bottomLeft) = float_lists_to_int_tuples([topLeft, topRight, bottomRight, bottomLeft])
     # Coordinates of quadrangle vertices in the source image.
     pts1 = np.float32([topLeft, topRight, bottomRight, bottomLeft])
     # Coordinates of the corresponding quadrangle vertices in the destination image.
@@ -114,11 +130,69 @@ def warp_quad_to_square(image, topLeft, topRight, bottomRight, bottomLeft):
          ])
     # Apply Perspective Transform Algorithm
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    image = cv2.warpPerspective(image, matrix, (image.shape[0], image.shape[1]))
+    image = cv2.warpPerspective(
+        image, matrix, (image.shape[0], image.shape[1]))
     return image
 
+def init_camera(image):
+    # Define camera intrinsic properties (how a camera maps 3D points in the world to 2D points in an image)
+    # Matrix can be though of as a rotation matrix concatenated with a translation matrix)
+    focal_length = [image.shape[0], image.shape[0]]             # In pixels
+    origin = np.array([image.shape[0]/2, image.shape[1]/2])    # principal point (the point that all rays converge) in pixels
+    camera_matrix = np.array(
+    [[focal_length[0], 0, origin[0]],
+    [0, focal_length[1], origin[1]],
+    [0, 0, 1]], dtype="double")
+    distCoeffs = np.zeros((4, 1))           # lens distortion of camera (None)
+    return focal_length, origin, camera_matrix, distCoeffs
+
+
 def process_image(image, arucoDict, arucoParams):
+    
+    focal_length, origin, camera_matrix, distCoeffs = init_camera(image)
+    
+    #Detect aruco tags
     (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+    if ids is None:
+        return
+    tags_count = len(ids.flatten())
+
+    cv2.aruco.drawDetectedMarkers(image, corners, ids)
+
+    marker_length = 45.00 # mm
+
+    poses = cv2.aruco.estimatePoseSingleMarkers(corners, marker_length, camera_matrix, distCoeffs)  
+    rvecs, tvecs, _objPoints = poses
+
+    for i in range(tags_count):
+        
+        (topLeft, topRight, bottomRight,bottomLeft) = corners[i][0]
+        id = ids[i]
+        size = abs(topRight[0] - topLeft[0])/2
+        rvec = rvecs[i][0] # Rotation with respect to camera
+        tvec = tvecs[i][0] # Translation with respect to camera
+        
+        if False:
+            # Draw the bounding box around the detected ArUCo tag
+            image = draw_quad(image, topLeft, topRight, bottomRight, bottomLeft)
+            # Draw a point at the center of the detected ArUCo tag
+            image = draw_quad_centerpoint(image, topLeft, topRight, bottomRight, bottomLeft)
+            # Draw the ID of the detected ArUCo tag
+            image = draw_quad_markerID(image, topLeft, topRight, bottomRight, bottomLeft, id)
+        # Draw the estimated pose of the tag
+        cv2.drawFrameAxes(image, camera_matrix, distCoeffs, rvec, tvec, size, thickness = 3)
+    return image
+
+
+"""    aruco_len = 1
+    for id in ids:
+        print("ID: ", id)
+        r_vec = np.array([])
+        t_vec = np.array([])
+        cv2.aruco.estimatePoseSingleMarkers(corners[0], aruco_len, camera_matrix, camera_dist)
+        image = cv2.drawFrameAxes(image, camera_matrix, camera_dist, r_vec, t_vec, 0.1)"""
+    
+"""    
     # Visualise ArUco markers
     # verify *at least* one ArUco marker was detected
     if len(corners) > 0:
@@ -127,23 +201,37 @@ def process_image(image, arucoDict, arucoParams):
         # loop over the detected ArUCo corners
         for (markerCorner, markerID) in zip(corners, ids):
             # Extract marker corners (which are always returned in order top-left, top-right, bottom-right, and bottom-left)
-            (topLeft, topRight, bottomRight, bottomLeft) = extract_marker_corners(markerCorner)
+            (topLeft, topRight, bottomRight,bottomLeft) = extract_marker_corners(markerCorner)
 
-            # draw the bounding box of the ArUCo detection on the image
-            image = draw_quad(image, topLeft, topRight, bottomRight, bottomLeft)
+            # Draw the bounding box around the detected ArUCo tag
+            image = draw_quad(image, topLeft, topRight,bottomRight, bottomLeft)
+            # Draw a point at the center of the detected ArUCo tag
             image = draw_quad_centerpoint(image, topLeft, topRight, bottomRight, bottomLeft)
+            # Draw the ID of the detected ArUCo tag
             image = draw_quad_markerID(image, topLeft, topRight, bottomRight, bottomLeft, markerID)
+            # Draw a frame axis for the detected ArUCo tag
+            #image = draw_aruco_frame_axis(image, topLeft, topRight, bottomRight, bottomLeft)
+            image = draw_frame_axis(image, topLeft, topRight, bottomRight, bottomLeft)
+       
+            
 
         # Choose the first tag detected for warping function
         (topLeft, topRight, bottomRight, bottomLeft) = extract_marker_corners(corners[0])
+
         # Warp perspective to make a tag square
-        image = warp_quad_to_square(image, topLeft, topRight, bottomRight, bottomLeft)
-    return image
+        if False:
+            image = warp_quad_to_square(image, topLeft, topRight, bottomRight, bottomLeft)
+    """
+    
+
+
+
 
 image = process_image(image, arucoDict, arucoParams)
 
 # Show image
-window_name = 'image'       # Name display window for image
+window_name = 'image'  # Name display window for image
 cv2.imshow("Image", image)  # Show the image using cv2.imshow() method
-cv2.waitKey(0)              # wait for user key press (necessary to avoid Python kernel form crashing)
-cv2.destroyAllWindows()     # closing all open windows (after key press)
+# wait for user key press (necessary to avoid Python kernel form crashing)
+cv2.waitKey(0)
+cv2.destroyAllWindows()  # closing all open windows (after key press)
